a527fd6c21b1cf663a22b458e25cc0bebda808f5
Takyon <bengilder99@gmail.com>
2026-07-15 14:49:24 +0100

    Added files with the challenge code and my solution code

---
+++ picoCTFgen.py
@@ -0,0 +1,33 @@
+key_part_static1_trial = "picoCTF{1n_7h3_kk3y_of_"
+key_part_dynamic1_trial = "xxxxxxxx"
+key_part_static2_trial = "}"
+key_full_template_trial = (
+    key_part_static1_trial + key_part_dynamic1_trial + key_part_static2_trial
+)
+hashed = "ba6c084a4d888e1f7c3b0fc71d61c4625708bd915b5e0e60eb73e1667251b567"
+
+
+def test(key, hashed):
+
+    # Just indexing to the correct position where the dynamic part starts
+    i = len(key_part_static1_trial)
+
+    # Make a string with the indexes at the correct positions
+    out_key = (
+        key[:i]
+        + hashed[4]
+        + hashed[5]
+        + hashed[3]
+        + hashed[6]
+        + hashed[2]
+        + hashed[7]
+        + hashed[1]
+        + hashed[8]
+        + key[i + 8 :]
+    )
+
+    # Return the key value
+    return out_key
+
+
+print(test(key_full_template_trial, hashed))
